af2233a2e5a11eeadc7ae33b331310ac82ed5385
860430 <bert.deterd2@stedin.net>
2021-04-13 15:42:15 +0200

    init

--- generator/index.js
+++ generator/index.js
@@ -9,7 +9,6 @@ module.exports = (api, opts) => {
     // adapted from https://github.com/Akryum/vue-cli-plugin-apollo/blob/master/generator/index.js#L68-L91
     api.onCreateComplete(() => {
 
-
         api.exitLog('Discordxx community: https://community.vuetifyjs.com')
         api.exitLog('Githubxx: https://github.com/vuetifyjs/vuetify')
         api.exitLog('Supportxx Vuetify: https://github.com/sponsors/johnleider')
