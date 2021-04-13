504688de4005161804301a3a933f27145d7a6241
860430 <bert.deterd2@stedin.net>
2021-04-13 16:14:49 +0200

    init

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,12 @@
 
 module.exports = (api, opts) => {
 
+    api.extendPackage({
+        dependencies: {
+            'saprfc': 'latest'
+        }
+    });
+
 
     api.render('./template')
 
@@ -9,8 +15,8 @@ module.exports = (api, opts) => {
     // adapted from https://github.com/Akryum/vue-cli-plugin-apollo/blob/master/generator/index.js#L68-L91
     api.onCreateComplete(() => {
 
-        api.exitLog('Discordxx community: https://community.vuetifyjs.com')
-        api.exitLog('Githubxx: https://github.com/vuetifyjs/vuetify')
-        api.exitLog('Supportxx Vuetify: https://github.com/sponsors/johnleider')
+        //api.exitLog('Discordxx community: https://community.vuetifyjs.com')
+        //api.exitLog('Githubxx: https://github.com/vuetifyjs/vuetify')
+        //api.exitLog('Supportxx Vuetify: https://github.com/sponsors/johnleider')
     })
 }
